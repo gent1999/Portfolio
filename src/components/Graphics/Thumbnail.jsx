aa45cf29373c7009f583d93b6e389c6a70151283
Mahmoud <tkhattab1999@gmail.com>
2024-02-23 09:01:08 -0500

    icon hovers

--- src/components/Graphics/Thumbnail.jsx
+++ src/components/Graphics/Thumbnail.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useInView } from 'react-intersection-observer';
 import photoshopIcon from '/icons/cib-adobe-photoshop.svg';
 import '../styles/Thumbnail.css';
@@ -9,20 +9,45 @@ const GraphicProjectThumbnail = ({ image, title }) => {
     threshold: 0.2, // Adjust the threshold as needed
   });
 
+  const [hoveredPhotoshop, setHoveredPhotoshop] = useState(false);
+
+  const handleMouseEnter = () => {
+    setHoveredPhotoshop(true);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredPhotoshop(false);
+  };
+
   return (
-    <div ref={ref} className={`pictures max-w-xs rounded overflow-hidden mx-2 my-4 transition duration-300 transform hover:scale-105 ${inView ? 'fade-in' : 'invisible'}`}>
+    <div
+      ref={ref}
+      className={`pictures max-w-xs rounded overflow-hidden mx-2 my-4 transition duration-300 transform hover:scale-105 ${inView ? 'fade-in' : 'invisible'}`}
+    >
+              {hoveredPhotoshop && (
+          <div className="message absolute fade-in bg-black text-white p-2 rounded-md text-sm bottom-11 mb-4 ml-1 opacity-100 pointer-events-auto transition-opacity duration-300 z-20">
+            {title}
+          </div>
+        )}
       <img className="w-full shadow-lg" src={image} alt={title} />
       <div className="px-6 py-4 relative">
-        {title !== 'Photoshop' && // Render the title if it's not "Photoshop"
+        {title !== 'Photoshop' && (
           <div className="text-xl mb-0 mt-2 relative z-10">{title}</div>
-        }
+        )}
         <div className="absolute top-0 left-0 mt-2 w-full h-8 bg-gradient-to-r from-purple-800 to-white-400"></div>
-        {title === 'Photoshop' && // Conditionally render the Photoshop icon
-          <img src={photoshopIcon} alt="Photoshop" className="inline-block" style={{ position: 'relative', top: '-7px', left: '-12px', width: '20px', filter: 'invert(1)'  }} />
-        }
+        {title === 'Photoshop' && (
+          <img
+            src={photoshopIcon}
+            alt="Photoshop"
+            className="inline-block"
+            style={{ position: 'relative', top: '-7px', left: '-12px', width: '20px', filter: 'invert(1)' }}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          />
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default GraphicProjectThumbnail;
--- src/components/Websites/Thumbnail.jsx
+++ src/components/Websites/Thumbnail.jsx
@@ -1,46 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faReact, faNode, faJsSquare } from '@fortawesome/free-brands-svg-icons';
+import { faDatabase } from '@fortawesome/free-solid-svg-icons'; // Import solid icon for PostgreSQL
 import postgresqlIcon from '/icons/cib-postgresql.svg';
 import threeJsIcon from '/icons/three-js-icon.svg';
 import tailwind from '/icons/tailwind-icon.svg';
 import '../styles/Thumbnail.css';
 
 const WebProjectThumbnail = ({ image, title, technologies }) => {
+  const [hoveredTech, setHoveredTech] = useState(null);
+
+  const handleMouseEnter = (tech) => {
+    setHoveredTech(tech);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredTech(null);
+  };
+
   return (
     <div className="web-pictures items-center max-w-xs lg:max-w-lg rounded overflow-hidden mx-2 transition duration-300 transform hover:scale-105 relative">
       <img className="w-full shadow-lg" src={image} alt={title} />
-      <div className="py-2 relative">
+      <div className="py-2 relative z-10"> {/* Set z-index to make sure the tooltip appears in front */}
         <div className="relative bg-gradient-to-r from-orange-800 to-white-400">
+        {hoveredTech && (
+            <div className="message absolute fade-in bg-black text-white p-2 rounded-md text-sm bottom-11 ml-1 opacity-100 pointer-events-auto transition-opacity duration-300 z-20"> {/* Ensure the tooltip appears in front */}
+              <span className="">{hoveredTech}</span>
+            </div>
+          )}
           <div className="text-2xl mb-0 ml-3 text-white">{title}</div>
           <p className="text-white text-sm py-1 ml-3">
-            {technologies.includes('React') && <FontAwesomeIcon icon={faReact} className="inline-block ml-0" style={{ width: '20px', height: '20px'}} />}
-            {technologies.includes('Node.js') && <FontAwesomeIcon icon={faNode} className="inline-block ml-2" style={{ width: '20px', height: '20px'}} />}
-            {technologies.includes('Express.js') && <FontAwesomeIcon icon={faJsSquare} className="inline-block ml-2" style={{ width: '20px', height: '20px'}} />}
-            {technologies.includes('PostgreSQL') &&
+            {technologies.includes('React') && (
+              <FontAwesomeIcon
+                icon={faReact}
+                className="inline-block ml-0 tech-icon"
+                style={{ width: '20px', height: '20px' }}
+                onMouseEnter={() => handleMouseEnter('React')}
+                onMouseLeave={handleMouseLeave}
+              />
+            )}
+            {technologies.includes('Node.js') && (
+              <FontAwesomeIcon
+                icon={faNode}
+                className="inline-block ml-2 tech-icon"
+                style={{ width: '20px', height: '20px' }}
+                onMouseEnter={() => handleMouseEnter('Node.js')}
+                onMouseLeave={handleMouseLeave}
+              />
+            )}
+            {technologies.includes('Express.js') && (
+              <FontAwesomeIcon
+                icon={faJsSquare}
+                className="inline-block ml-2 tech-icon"
+                style={{ width: '20px', height: '20px' }}
+                onMouseEnter={() => handleMouseEnter('Express.js')}
+                onMouseLeave={handleMouseLeave}
+              />
+            )}
+            {technologies.includes('PostgreSQL') && (
               <img
                 src={postgresqlIcon}
                 alt="PostgreSQL"
-                className="inline-block ml-2"
-                style={{ position: 'relative', top: '-4px', width: '19px', filter: 'invert(1)' }} // Adjust position, size, and color
+                className="inline-block ml-2 tech-icon"
+                style={{ position: 'relative', top: '-4px', width: '20px', filter: 'invert(1)' }}
+                onMouseEnter={() => handleMouseEnter('PostgreSQL')}
+                onMouseLeave={handleMouseLeave}
               />
-            }
-            {technologies.includes('Three.js') &&
+            )}
+            {technologies.includes('Three.js') && (
               <img
                 src={threeJsIcon}
                 alt="Three.js"
-                className="inline-block ml-2"
-                style={{ position: 'relative', top: '-4px', width: '20px' }} // Adjust position and size
+                className="inline-block ml-2 tech-icon"
+                style={{ position: 'relative', top: '-4px', width: '20px' }}
+                onMouseEnter={() => handleMouseEnter('Three.js')}
+                onMouseLeave={handleMouseLeave}
               />
-            }
-            {technologies.includes('Tailwind') &&
+            )}
+            {technologies.includes('Tailwind') && (
               <img
                 src={tailwind}
                 alt="Tailwind"
-                className="inline-block ml-2"
-                style={{ position: 'relative', top: '-6px', width: '20px', filter: 'invert(1)' }} // Adjust position and size
+                className="inline-block ml-2 tech-icon"
+                style={{ position: 'relative', top: '-6px', width: '20px', filter: 'invert(1)' }}
+                onMouseEnter={() => handleMouseEnter('Tailwind CSS')}
+                onMouseLeave={handleMouseLeave}
               />
-            }
+            )}
           </p>
         </div>
       </div>
